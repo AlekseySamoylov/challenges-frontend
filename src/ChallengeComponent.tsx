f758ea61980b998185ce35bd58365b7fd1c61b55
alekseysamoylov <alekseysamoylov89@gmail.com>
2021-04-25 22:25:11 +0400

    Start adding components

---
+++ src/ChallengeComponent.tsx
@@ -0,0 +1,13 @@
+class ChallengeComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            a: '', b: '',
+            user: '',
+            message: '',
+            guess: 0
+        }
+    }
+}
+
+export default ChallengeComponent
